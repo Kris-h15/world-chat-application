09fcace9b1b9361858a9260b8ab947e5cd44c68f
Aman Kumar Jha <amanjha2470@gmail.com>
2026-09-18 22:04:01 +0530

    socket port update

--- frontend_whatsapp/src/socket.js
+++ frontend_whatsapp/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
 export default function connect_socket() {
-  return io("http://localhost:3000");
+  // return io("http://localhost:3000");
+  return io("https://world-chat-application-git-main-aman-0f6d.vercel.app/");
 }

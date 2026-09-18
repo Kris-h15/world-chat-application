eba03d1d2cad9758eebb9cee01679159fe5aad07
Aman Kumar Jha <amanjha2470@gmail.com>
2026-09-18 23:01:04 +0530

    4th commit

--- backend/server.js
+++ backend/server.js
@@ -66,6 +66,6 @@ io.on("connection", (socket) => {
 });
 
 // -----------------------------------------------------------
-server.listen(port1, "0.0.0.0", () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
